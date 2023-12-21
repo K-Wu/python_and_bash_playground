db4080c04ec186500aacb4b6c9cbce04bba851c5
Kun Wu <11816012+K-Wu@users.noreply.github.com>
2023-12-21 13:12:23 -0600

    upd

--- try_init_once/init_once.py
+++ try_init_once/init_once.py
@@ -1,5 +1,8 @@
 import random
 
+
+# "The canonical way to share information across modules within a single program is to create a special module (often called config or cfg)." https://docs.python.org/3/faq/programming.html#how-do-i-share-global-variables-across-modules
+# Reference on how python caches modules so that they are only loaded once: https://docs.python.org/3/library/sys.html#sys.modules
 global var
 var = random.randint(0, 100)
 
