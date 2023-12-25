cffe7d37bbdd32dfdf79da8345f2b86617263608
Kun Wu <11816012+K-Wu@users.noreply.github.com>
2023-12-25 15:49:14 -0600

    upd

---
+++ try_inheritance_auto_virtual_function.py
@@ -0,0 +1,55 @@
+# From https://stackoverflow.com/a/37290710/5555077
+class Foo(object):
+    def __init__(self):
+        print("foo init")
+        self.foo_func()
+
+    def foo_func(self):
+        print("making bar")
+        self.x = Bar()
+
+    def foo_bar_func(self):
+        self.x.bar_func()
+
+
+class Fighters(Foo):
+    def __init__(self):
+        print("fighters init")
+        Foo.__init__(self)
+
+    def func1(self):
+        Foo.some_attribute
+
+    def func2(self):
+        Foo.someother_func()
+
+
+class Bar(object):
+    def __init__(self):
+        print("bar init")
+
+    def bar_func(self):
+        # some stuff here
+        print("bar bar_func")
+
+
+class MyFighters(Fighters):
+    def foo_func(self):
+        print("making mybar")
+        self.x = MyBar()
+
+
+class MyBar(Bar):
+    def bar_func(self):
+        print("mybar bar_func")
+
+
+f2 = MyFighters()
+f2.foo_bar_func()
+
+# Output:
+# fighters init
+# foo init
+# making mybar
+# bar init
+# mybar bar_func
