ef9f8cdcf9518d2f76ede2e680d48afcbb0ed6ee
Kun Wu <11816012+K-Wu@users.noreply.github.com>
2023-12-27 10:12:03 -0600

    upd

---
+++ try_module_this/client_a.py
@@ -0,0 +1,4 @@
+# From https://stackoverflow.com/a/35904211/5555077
+import db
+
+db.initialize_db("mongo")
---
+++ try_module_this/db.py
@@ -0,0 +1,19 @@
+# From https://stackoverflow.com/a/35904211/5555077
+import sys
+
+# this is a pointer to the module object instance itself.
+this = sys.modules[__name__]
+
+# we can explicitly make assignments on it
+this.db_name = None
+
+
+def initialize_db(name):
+    if this.db_name is None:
+        # also in local function scope. no scope specifier like global is needed
+        this.db_name = name
+        # also the name remains free for local use
+        db_name = "Locally scoped db_name variable. Doesn't do anything here."
+    else:
+        msg = "Database is already initialized to {0}."
+        raise RuntimeError(msg.format(this.db_name))
