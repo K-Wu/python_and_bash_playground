768d06ce62345383f429db564acd768ee280d495
Kun Wu <11816012+K-Wu@users.noreply.github.com>
2024-04-14 01:41:18 -0500

    upd

---
+++ try_destructor.py
@@ -0,0 +1,16 @@
+# Python program to illustrate destructor
+class Employee:
+
+    # Initializing
+    def __init__(self):
+        print("Employee created.")
+
+    # Deleting (Calling destructor)
+    def __del__(self):
+        print("Destructor called, Employee deleted.")
+
+
+obj = Employee()
+obj2 = obj
+del obj
+del obj2
