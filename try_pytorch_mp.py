58dfde20066acd6607d53b7c20e0f5764448721d
Kun Wu <11816012+K-Wu@users.noreply.github.com>
2024-06-10 15:05:09 -0500

    upd

--- try_pytorch_mp.py
+++ try_pytorch_mp.py
@@ -3,7 +3,7 @@ import torch.multiprocessing as mp
 import torch
 
 
-def foo(q, worker, tl):
+def foo(value, q, worker, tl):
     tl[worker] += (worker + 1) * 1000
     if worker == 0:
         dummy = torch.randn(2, device="cuda:0")
@@ -11,9 +11,16 @@ def foo(q, worker, tl):
             12345  # This property will not be passed to the other process
         )
         q.put((dummy, 12345))
+        # del value.value["producer"]
+        with value.get_lock():
+            # From https://stackoverflow.com/questions/74910247/python-multiprocessing-sharing-variables-between-processes
+            value.value -= 1
     else:
         dummy, timestamp = q.get()
         print("dummy=", timestamp)
+        # value.value["done"] = 1
+        with value.get_lock():
+            value.value += 1
 
 
 if __name__ == "__main__":
@@ -27,9 +34,11 @@ if __name__ == "__main__":
     print(tl)
 
     q = mp.Queue()
+    # Not working: value = mp.Value(dict, {"producer": 1})
+    value = mp.Value("i", 0)
 
-    p0 = mp.Process(target=foo, args=(q, 0, tl))
-    p1 = mp.Process(target=foo, args=(q, 1, tl))
+    p0 = mp.Process(target=foo, args=(value, q, 0, tl))
+    p1 = mp.Process(target=foo, args=(value, q, 1, tl))
     p0.start()
     p1.start()
     p0.join()
